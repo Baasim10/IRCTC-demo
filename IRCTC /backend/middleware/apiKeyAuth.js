321bb8fcc0530d6a883cddf15ff2ec527df258bf
Baasim10 <112801450+Baasim10@users.noreply.github.com>
2024-08-05 23:33:41 +0530

    Delete IRCTC  directory

--- IRCTC /backend/middleware/apiKeyAuth.js
+++
@@ -1,9 +0,0 @@
-const checkApiKey = (req, res, next) => {
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey !== process.env.ADMIN_API_KEY) {
-        return res.status(403).json({ error: 'Forbidden' });
-    }
-    next();
-};
-
-module.exports = checkApiKey;
